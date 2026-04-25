7732f9f9f2d571b14ab4572cfba839256f00f045
Austin Kim <154750011+AsphyxiaUwO@users.noreply.github.com>
2026-04-24 18:48:05 -0700

    Add files via upload

---
+++ starter.py
@@ -0,0 +1,29 @@
+# Austin Kim, Saanvi Sakthivel, & Yuna Jung
+# CS5 Final Project: Picobot
+# 4/24/2026
+
+import random
+
+HEIGHT = 25
+WIDTH = 25
+NUMSTATES = 5
+
+POSSIBLE_SURROUNDINGS = ['xxxx','Nxxx','NExx','NxWx','xxxS','xExS','xxWS','xExx','xxWx']
+
+class Program:
+    def __init__(self):
+        self.rules = {}
+    
+    def __repr__(self):
+        unsortedKeys = list(self.rules.keys())
+        sortedKeys = sorted(unsortedKeys)
+        s = ''
+        for key in sortedKeys:
+            s += " ".join([str(key[0]), str(key[1]), "->", str(self.rules[key][0]), str(self.rules[key][1])]) + "\n"
+        return s
+
+    def randomize(self):
+        for state in range(NUMSTATES):
+            for surr in POSSIBLE_SURROUNDINGS:
+                dirs = "".join([s for s in 'NEWS' if s not in surr])
+                self.rules[(state, surr)] = (random.choice(dirs), random.randrange(0, NUMSTATES))
